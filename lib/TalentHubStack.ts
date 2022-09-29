ece0e9fd614d9eae288db5b476859cb58c5d48e0
Peter Ayeni <ayenicology@gmail.com>
2022-09-29 09:29:39 +0100

    feat: add hello endpoint

--- lib/TalentHubStack.ts
+++ lib/TalentHubStack.ts
@@ -1,10 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda  from 'aws-cdk-lib/aws-lambda'
+import { Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 
 export class TalentHubStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    
+    const hello = new lambda.Function(this, 'HelloHandler',{
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('services'),
+      handler: 'hello.handler'
+    })
   }
 }
