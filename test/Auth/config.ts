cf26161341da48f686c5550b841e8be8b4177a34
Peter Ayeni <ayenicology@gmail.com>
2022-09-30 00:55:30 +0100

    update test auth credentials

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -1,8 +1,8 @@
 export const config = {
   REGION: "eu-west-3",
-  USER_POOL_ID: "eu-west-3_la6H3N587",
-  APP_CLIENT_ID: "5djjpfosqksq4659h0rg97a3e9",
-  IDENTITY_POOL_ID: "eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599",
+  USER_POOL_ID: "eu-west-3_A3WtNSqoh",
+  APP_CLIENT_ID: "27outa84n19g4l88l5ce16ft0j",
+  IDENTITY_POOL_ID: "eu-west-3:e21b1806-eba1-44c9-b144-1283724c59a8",
   TEST_USER_NAME: "test",
   TEST_USER_PASSWORD: "A@6q8esd3ap",
 };
